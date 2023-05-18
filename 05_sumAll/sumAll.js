b02f542371e49fd952d0cfa3f193f0780f14eb63
kratospidey <67730931+Kratospidey@users.noreply.github.com>
2023-05-19 01:04:54 +0530

    finish 05sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (low, high) {
+	if (low < 0 || high < 0) {
+		return `ERROR`;
+	}
+	if (typeof low !== `number` || typeof high !== `number`) {
+		return `ERROR`;
+	}
+	if (low > high) {
+		let temp = low;
+		low = high;
+		high = temp;
+	}
+	let sum = 0;
+	for (let num = low; num <= high; num++) {
+		sum += num;
+	}
+	return sum;
 };
 
 // Do not edit below this line
