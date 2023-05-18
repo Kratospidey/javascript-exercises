d374608aaf23d614ac3fe519abed72c4cbeaa1fb
kratospidey <67730931+Kratospidey@users.noreply.github.com>
2023-05-19 00:05:22 +0530

    finish 02repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+	if (num < 0) {
+		return `ERROR`;
+	}
+	phrase = "";
+	for (let i = 1; i <= num; i++) {
+		phrase += word;
+	}
+	return phrase;
 };
 
 // Do not edit below this line
